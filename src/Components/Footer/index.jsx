29c5b8f1bd0ab6e31f7afcb247f2b77392eda9a2
Sankarsan Kampa <me@sankarsankampa.com>
2018-04-14 08:46:56 +0000

    Added Footer (React) component

---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './index.css';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className='copyright'>
+          Copyright &copy; 2018 - The Bastion Bot Project
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
